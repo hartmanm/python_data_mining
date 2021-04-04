0319d6a823b0c7dc794d3942c19a6c48a38476c9
hartmanm <hartman.m@gmail.com>
2021-04-04 18:21:48 +0000

    working changes

--- assignment_4.cpp
+++ assignment_4.cpp
@@ -22,42 +22,53 @@ int main(){
 // N The number of data points (lines) following the first line .
 // K The number of output clusters .
 // M The cluster similarity measure to be used.  for single link,  for complete link,  for average link .
-    
+
 
 
 // Euclidean(LAT1, LAT2, LONG1, LONG2);
 
 
 
-    vector<cluster> clusters; 
 
-    string output;
-    int cluster_count=0;
-    vector<float> points_x;
-    vector<float> points_y;
 
+vector<cluster> clusters; 
+
+string output;
+int cluster_count=0;
+int cluster_iterator=0;
+vector<float> points_x;
+vector<float> points_y;
+float min_distance=1000;
+
+float distance;
+
+
+vector<int> cluster_assigned;
+
+int distance_index_i;
+int distance_index_j;
 /*
-    vector<float> ordered_points_x;
-    vector<float> ordered_points_y;
-    float distance;
-    int distance_index;
-    vector<float> cluster_assigned;
+vector<float> ordered_points_x;
+vector<float> ordered_points_y;
+
+vector<float> cluster_assigned;
 */
 
-    int N;
-    int K;
-    int M;
-    cin >> N >> K >> M;
+int N;
+int K;
+int M;
+cin >> N >> K >> M;
 
 
-    for(int i=0;i<N;i++){
-    float x=0;
-    float y=0;
-    cin >> x >> y;
-    cluster_count++;
-    points_x.push_back(x);
-    points_y.push_back(y);
-    } //    for(int i=0;i<N;i++){
+for(int i=0;i<N;i++){
+float x=0;
+float y=0;
+cin >> x >> y;
+cluster_count++;
+points_x.push_back(x);
+points_y.push_back(y);
+cluster_assigned.push_back(0.0);
+} //    for(int i=0;i<N;i++){
 
 output="number of clusters: ";
 output.append(to_string(cluster_count));
@@ -71,7 +82,7 @@ output="K The number of output clusters: ";
 output.append(to_string(K));
 Output(output);
 
-output="M The cluster similarity measure to be used.  0 for single link,  1 for complete link,  2 for average link : ";
+output="M The cluster similarity measure to be used.  0 for single link (min),  1 for complete link (max),  2 for average link (mean): ";
 output.append(to_string(M));
 Output(output);
 
@@ -84,8 +95,7 @@ x=points_x.at(i);
 y=points_y.at(i);
 output="cluster: ";
 output.append(to_string(i));
-Output(output);
-output="x,y: ";
+output.append("  x,y: ");
 output.append(to_string(x));
 output.append(",");
 output.append(to_string(y));
@@ -94,11 +104,64 @@ Output(output);
 
 
 
+while(cluster_count > K)
+{
+
+// Single link: min
+if(M==0){
+distance_index_i=0;
+distance_index_j=0;
+for(int j=0;j<N;j++){
+distance=1000;
+min_distance=1000;
+float x=0;
+float y=0;
+x=points_x.at(j);
+y=points_y.at(j);
+for(int i=0;i<N;i++){
+float x2=0;
+float y2=0;
+if(i!=j){
+x2=points_x.at(i);
+y2=points_y.at(i);
+distance=Euclidean(x, x2, y, y2);
+if(distance < min_distance){
+min_distance=distance;
+distance_index_i=i;
+distance_index_j=j;
+} //    if(distance < min_distance){
+} //    if(i!=j){
+} //    for(int i=0;i<N;i++){
+output="";
+output.append(to_string(distance));
+output.append(" ");
+output.append(to_string(distance_index_j));
+output.append(to_string(distance_index_i));
+Output(output);
+cluster_assigned.at(distance_index_j)=cluster_iterator;
+cluster_assigned.at(distance_index_i)=cluster_iterator;
+} //    for(int j=0;j<N;j++){
 
 
 
 
 
+} //    if(M==0){
+
+
+cluster_count--;
+cluster_iterator++;
+
+} //    while(cluster_count > K)
+
+
+for(int i=0;i<N;i++){
+int x=0;
+x=cluster_assigned.at(i);
+output="";
+output.append(to_string(x));
+Output(output);
+} //    for(int i=0;i<N;i++){
 
 return 0;
 
@@ -355,4 +418,4 @@ float THE_INPUT=THE_LAT+THE_LONG;
 return sqrt(THE_INPUT);
 };
 
-void Output(string the_output){cout<<endl<<the_output<<endl;}
+void Output(string the_output){cout<<the_output<<endl;}
